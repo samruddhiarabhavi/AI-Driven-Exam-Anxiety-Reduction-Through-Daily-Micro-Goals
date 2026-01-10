bf6238b0186db9db9d5e101173b94ac8b767cb77
samruddhiarabhavi <samruddhiarabhavi@gmail.com>
2026-01-10 15:34:44 +0530

    Add project files and ignore uploads

--- project_root/models/timetable.py
+++
@@ -1,8 +0,0 @@
-from models import db
-from datetime import datetime
-
-class TimetableHistory(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    student_id = db.Column(db.Integer, nullable=False)
-    date = db.Column(db.Date, default=datetime.utcnow)
-    timetable_json = db.Column(db.Text)
